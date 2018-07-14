8507a0b409d1c29e8013129abad9f85708a2ccad
Danny Guo <dannyguo91@gmail.com>
2018-07-14 18:10:16 -0400

    Rollup third party dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
     input: './src/index.js',
     output: {
         file: 'wind-tunnel.js',
         format: 'es'
     },
-    external: ['p5beh']
+    external: ['p5beh'],
+    plugins: [
+        resolve(),
+        commonjs()
+    ]
 };
