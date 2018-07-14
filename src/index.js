f5d8df67ece25ec09583103c113804520d02740b
Azeem Bande-Ali <a.bandeali@gmail.com>
2018-07-14 18:22:12 -0400

    Add foil rendering.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@
  */
 
 import P5Behavior from 'p5beh';
+import naca from 'naca-four-digit-airfoil';
 
+const NUM_POINTS = 100;
+const CHORD_LENGTH = 100;
+const airfoil = naca('3418', CHORD_LENGTH);
 const pb = new P5Behavior();
 /* this == pb.p5 == p */
 
@@ -27,14 +31,39 @@ pb.draw = function (floor, p) {
   // console.log('hello', floor, p);
   this.clear();
 
-  // for (let u of floor.users) {
-    // pb.drawUser(u);
-  // }
+
+  for (let u of floor.users) {
+    this.fill('red');
+    this.stroke('red');
+    this.strokeWeight(1);
+    this.beginShape();
+    
+    var flip = []
+    let xOffset = u.x - CHORD_LENGTH/2;
+    let yOffset = u.y; 
+    for (let i = 0; i < NUM_POINTS; ++i)
+    {
+      let lookupX = CHORD_LENGTH*i/NUM_POINTS;
+      let points = airfoil.evaluate(lookupX);
+      let x = points[0];
+      let y = points[1];
+
+      this.vertex(xOffset + x, yOffset + y);
+      flip.push(points);
+    }
+
+    flip.reverse();
+    for (let i = 0; i < NUM_POINTS; i++)
+    {
+      this.vertex(flip[i][2] + xOffset, flip[i][3] + yOffset);
+    }
+
+    this.endShape(this.CLOSE);
+  }
+
 
   this.fill(20, 20, 60, 60);
-
   this.noStroke();
-
   // pb.drawSensors(floor.sensors);
 };
 
@@ -43,7 +72,7 @@ export const behavior = {
   init: pb.init.bind(pb),
   frameRate: 'animate',
   render: pb.render.bind(pb),
-  numGhosts: 0
+  numGhosts: 3
 };
 
 export default behavior;
